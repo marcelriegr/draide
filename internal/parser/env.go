9e862cb062493ab3e8dc269bf6158f819c9a895d
Marcel Rieger <marcel@blim.us>
2020-09-21 06:45:21 +0200

    ✨ throw error when a required environment variable not found

--- internal/parser/env.go
+++ internal/parser/env.go
@@ -3,12 +3,20 @@ package parser
 import (
 	"os"
 	"regexp"
+
+	"github.com/marcelriegr/draide/pkg/ui"
 )
 
 // Env replaces all variable starting with a $ (dollar sign) or # (number sign) character inside a string with the corresponding environment variable
 func Env(str string) string {
 	pattern := regexp.MustCompile(`[\$#]\w+`)
-	return pattern.ReplaceAllStringFunc(str, func(s string) string {
-		return os.Getenv(s[1:])
+	return pattern.ReplaceAllStringFunc(str, func(envVar string) string {
+		val := os.Getenv(envVar[1:])
+
+		if val == "" {
+			ui.ErrorAndExit(1, "Cannot resolve environment variable %s", envVar)
+		}
+
+		return val
 	})
 }
